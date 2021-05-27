8c2e39d69c2bb4f116ce384c2b9eb6d84a92152e
syed <novelcode0@gmail.com>
2021-05-27 20:40:06 +0530

    Lec 216 Coding Challenge 3

--- starter/script.js
+++ starter/script.js
@@ -279,3 +279,33 @@ const mike = new Student('Mike', 2020, 'Computer Science');
 Student.prototype.constructor = Student;
 
 // console.dir(Student.prototype.constructor);
+
+// Lec 216 Coding Challenge 3
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed = this.speed + 20;
+  this.charge = this.charge - 1;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}`
+  );
+};
+
+const elon = new EV('Tesla', 120, 23);
+
+elon.accelerate();
+elon.brake();
+elon.accelerate();
+elon.chargeBattery(90);
+elon.accelerate();
+elon.accelerate();
