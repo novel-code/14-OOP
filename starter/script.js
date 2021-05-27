fb78b8c313658c76ea22b3ad401bdb89de6ef6ee
syed <novelcode0@gmail.com>
2021-05-27 17:38:10 +0530

    Coding Challenge 2 completed

--- starter/script.js
+++ starter/script.js
@@ -208,3 +208,32 @@ steven.clacAge();
 const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1979);
 sarah.clacAge();
+
+// Lec 214 Coding challenge 2
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed = this.speed + 10;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+  }
+  brake() {
+    this.speed = this.speed - 5;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+  }
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl('Ford', 120);
+// console.log(ford.speedUS);
+// ford.accelerate();
+// ford.brake();
+ford.speedUS = 50;
+// console.log(ford);
