e5f2106c98791770bf7e7fccf05bb3eb9dc51602
syed <novelcode0@gmail.com>
2021-05-27 17:02:58 +0530

    Lec213 3rd way of Prot Inheritance (Object.create)

--- starter/script.js
+++ starter/script.js
@@ -185,3 +185,26 @@ Person2.hey = function () {
 // Person2.hey();
 
 // Lec 213 3rd way of prototypal inheritance
+
+const PersonProto = {
+  clacAge() {
+    // console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+// console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+
+steven.clacAge();
+// console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.clacAge();
