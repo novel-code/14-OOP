b6cb1e8ea07efd095daf75a80a5e5de79077f957
syed <novelcode0@gmail.com>
2021-05-29 00:14:58 +0530

    Lec 221 encapsulation

--- starter/script.js
+++ starter/script.js
@@ -356,46 +356,81 @@ jay.init('Jay', 2010, 'Computer Science');
 // jay.introduce();
 // jay.clacAge();
 
+// Lec 221
+
+// 1)Public fields
+// 2)Private fields
+// 3)Public methods
+// 4)Private methods
+// (there is also the static version)
+
 // Lec 219 Another class example
 class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
-    this.locale = navigator.language;
+    // Protected Property
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
+  // 3)Public methods
   // Public interface
+  getMovements() {
+    return this.#movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this.#movements.push(val);
   }
   withdraw(val) {
     this.deposit(-val);
   }
-  approveLoan(val) {
-    return true;
-  }
+
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+  // 4)Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
   }
 }
 
 const acc1 = new Account('Jonas', 'EUR', 1111);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-50);
+// acc1._movements.push(250);
+// acc1._movements.push(-50);
 acc1.deposit(250);
 acc1.withdraw(40);
 acc1.requestLoan(1000);
-acc1.approveLoan(300);
+console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
 
 // Lec 220 Ecapsulation: Protected Properties and Methods
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// acc1.#approveLoan(300);
+
+Account.helper();
+
+// Lec 222 Chaining Methods
