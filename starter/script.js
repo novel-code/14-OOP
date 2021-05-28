249bbc73c4e4319180234be37d550e0fe83b42d6
syed <novelcode0@gmail.com>
2021-05-28 14:58:08 +0530

    Lec 219

--- starter/script.js
+++ starter/script.js
@@ -104,7 +104,7 @@ class PersonCl {
   // Instance methods
   // Methods will be added to .prototype property
   clacAge() {
-    console.log(2037 - this.birthYear);
+    // console.log(2037 - this.birthYear);
   }
   greet() {
     // console.log(`Hey ${this.firstName}`);
@@ -335,8 +335,8 @@ class StudentCl extends PersonCl {
 
 // const martha = new StudentCl('Martha Jones', 2012); //(doesn't need constructor fn in student class)
 const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
-martha.introduce();
-martha.clacAge();
+// martha.introduce();
+// martha.clacAge();
 
 // Lec 218
 
@@ -353,7 +353,49 @@ StudentProto.introduce = function () {
 const jay = Object.create(StudentProto);
 
 jay.init('Jay', 2010, 'Computer Science');
-jay.introduce();
-jay.clacAge();
+// jay.introduce();
+// jay.clacAge();
 
 // Lec 219 Another class example
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+  approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-50);
+acc1.deposit(250);
+acc1.withdraw(40);
+acc1.requestLoan(1000);
+acc1.approveLoan(300);
+
+console.log(acc1);
+console.log(acc1.pin);
+
+// Lec 220 Ecapsulation: Protected Properties and Methods
